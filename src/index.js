e13f705fc46bb073ee74404b7d8aa170384d53ba
Artur Wojciechowski <me@iama.re>
2019-07-16 19:07:59 +0200

    Add drone docker build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
-const handlebars = require('handlebars')
-const fs = require('fs')
-const path = require('path')
-const node_ssh = require('node-ssh')
+const handlebars = require("handlebars")
+const fs = require("fs")
+const path = require("path")
+const node_ssh = require("node-ssh")
 const ssh = new node_ssh()
 
-const format = (str = '') => {
-    return handlebars.compile(str)(process.env)
+const format = (str = "") => {
+  return handlebars.compile(str)(process.env)
 }
 
 const USERNAME = format(process.env.PLUGIN_USERNAME)
@@ -16,34 +15,36 @@ const SCRIPT = process.env.PLUGIN_SCRIPT
 const ENV = JSON.parse(process.env.PLUGIN_ENV)
 
 for (let [key, value] of Object.entries(ENV)) {
-    process.env[key] = format(value)
+  process.env[key] = format(value)
 }
 
-async function main () {
-    const connection = await ssh.connect({
-        host: HOST,
-        username: USERNAME,
-        privateKey: PRIVATE_KEY_PATH,
-    })
+async function main() {
+  await ssh.connect({
+    host: HOST,
+    username: USERNAME,
+    privateKey: PRIVATE_KEY_PATH
+  })
 
-    const lines = SCRIPT.split(',')
+  const lines = SCRIPT.split(",")
 
-    for (let line of lines) {
-        const formattedLine = format(line)
+  for (let line of lines) {
+    const formattedLine = format(line)
 
-        console.log(`${HOST} $ ${formattedLine}`)
+    console.log(`[${USERNAME}@${HOST}] $ ${formattedLine}`)
 
-        const {stdout, stderr} = await ssh.execCommand(formattedLine)
+    const { stdout, stderr, code } = await ssh.execCommand(formattedLine)
 
-        if (stderr) {
-            throw stderr
-        } else {
-            console.log(stdout)
-        }
+    if (code !== 0) {
+      throw stderr
+    } else {
+      for (let line of stdout.split("\n")) {
+        console.log(`[ssh] ${line}`)
+      }
     }
+  }
 }
 
 main().catch(error => {
-    console.log(error)
-    process.exit(1)
+  console.log(error)
+  process.exit(1)
 })
