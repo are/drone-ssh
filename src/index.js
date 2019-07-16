e182604cda439c486fb568834d7da1e8df15d226
Artur Wojciechowski <artur.wojciechowski@auto1.com>
2019-07-16 17:05:42 +0200

    asdjfnasdf

--- src/index.js
+++ src/index.js
@@ -18,17 +18,19 @@ const ssh = new node_ssh()
   // - ssh dokku@dokku.are1000.dev tar:from "$APP_NAME" "https://git.are1000.dev/are/blog.iama.re/releases/download/$DRONE_TAG/release.tar"
   // - ssh dokku@dokku.are1000.dev letsencrypt "$APP_NAME"
 
-console.log(process.env)
-
-const format = (str) => {
-
-    return handlebars.compile(str)(Object.assign({}, process.env))
+const format = (str = '') => {
+    return handlebars.compile(str)(process.env)
 }
 
 const USERNAME = format(process.env.PLUGIN_USERNAME)
 const HOST = format(process.env.PLUGIN_HOST)
 const PRIVATE_KEY_PATH = format(process.env.PLUGIN_PRIVATE_KEY_PATH)
 const SCRIPT = process.env.PLUGIN_SCRIPT
+const ENV = JSON.parse(process.env.PLUGIN_ENV)
+
+for (let [key, value] of Object.entries(ENV)) {
+    process.env[key] = format(value)
+}
 
 async function main () {
     const connection = await ssh.connect({
