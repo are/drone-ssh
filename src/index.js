298287a3fd7c609f6d5575a556e30d35a8355d3b
Artur Wojciechowski <me@iama.re>
2019-07-16 21:34:58 +0200

    new

--- src/index.js
+++ src/index.js
@@ -19,37 +19,14 @@ for (let [key, value] of Object.entries(ENV)) {
   process.env[key] = format(value)
 }
 
-const STR_REGEX = /^\"([^\"]+)\"$/
-
-const stream = exec(
-  '/bin/sh',
-  {
-    host: HOST,
-    user: USERNAME,
-    key: PRIVATE_KEY_PATH
-  },
-  function(err, stdout, stderr) {
-    if (err) {
-      console.log(`[!!!] ${err.message}`)
-      console.log(stdout, stderr)
-      process.exit(1)
-    }
-  }
-)
-// await ssh.connect({
-//   host: HOST,
-//   username: USERNAME,
-//   privateKey: PRIVATE_KEY_PATH
-// })
-
 const lines = SCRIPT.split(',')
 let currentIdx = 0
 
-function cont() {
+function go() {
   const line = lines[currentIdx]
+  currentIdx += 1
 
   if (line === undefined) {
-    stream.end()
     process.exit(0)
   }
 
@@ -57,18 +34,29 @@ function cont() {
 
   console.log(`[${USERNAME}@${HOST}] $ ${formattedLine}`)
 
-  stream.write(formattedLine + '\n')
+  const stream = exec(
+    formattedLine,
+    {
+      host: HOST,
+      user: USERNAME,
+      key: PRIVATE_KEY_PATH
+    },
+    function(err, stdout, stderr) {
+      if (err) {
+        console.log(`[!!!] ${err.message}`)
+        console.log(stdout, stderr)
+        process.exit(1)
+      } else {
+        go()
+      }
+    }
+  )
 
-  currentIdx += 1
+  stream.on('data', chunk => {
+    const output = chunk.toString('utf8')
+
+    for (let line of output.split('\n')) {
+      console.log(`[ssh] ${line}`)
+    }
+  })
 }
-
-stream.on('data', chunk => {
-  const output = chunk.toString('utf8')
-  for (let line of output.split('\n')) {
-    console.log(`[ssh] ${line}`)
-  }
-
-  cont()
-})
-
-cont()
