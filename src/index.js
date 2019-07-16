b1442bd0e23e2fbad5bd6355a606a5940fe14fda
Artur Wojciechowski <artur.wojciechowski@auto1.com>
2019-07-16 17:08:34 +0200

    Final version

--- src/index.js
+++ src/index.js
@@ -5,19 +5,6 @@ const path = require('path')
 const node_ssh = require('node-ssh')
 const ssh = new node_ssh()
 
-  // - apk update
-  // - apk add openssh
-  // - apk add git
-  // - mkdir -p ~/.ssh/
-  // - ssh-keyscan -H dokku.are1000.dev >> ~/.ssh/known_hosts
-  // - eval $(ssh-agent -s)
-  // - npx -q gitea-secret -h git.are1000.dev -t "$GITEA_TOKEN" -r are/secrets -k ssh-keys/dokku-drone/private | ssh-add -
-  // - export APP_NAME="$DRONE_TAG-$DOMAIN"
-  // - ssh dokku@dokku.are1000.dev apps:create "$APP_NAME"
-  // - ssh dokku@dokku.are1000.dev domains:add "$APP_NAME" "$APP_NAME"
-  // - ssh dokku@dokku.are1000.dev tar:from "$APP_NAME" "https://git.are1000.dev/are/blog.iama.re/releases/download/$DRONE_TAG/release.tar"
-  // - ssh dokku@dokku.are1000.dev letsencrypt "$APP_NAME"
-
 const format = (str = '') => {
     return handlebars.compile(str)(process.env)
 }
@@ -29,7 +16,6 @@ const SCRIPT = process.env.PLUGIN_SCRIPT
 const ENV = JSON.parse(process.env.PLUGIN_ENV)
 
 for (let [key, value] of Object.entries(ENV)) {
-    console.log(key, value, format(value))
     process.env[key] = format(value)
 }
 
